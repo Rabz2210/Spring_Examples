28f8ae6b63d5196cbb102836a8edd18e6a193ee9
rabbani <topspin2210@gmail.com>
2017-11-25 13:27:50 +0530

    Setter injection through properties File

--- SpringDemoThree/src/com/Spring/TestSpring.java
+++ SpringDemoThree/src/com/Spring/TestSpring.java
@@ -6,7 +6,7 @@ public class TestSpring {
 	public static void main(String[] args) {
 		
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-		Cricket obj = context.getBean("cC", Cricket.class);
+		baseBall obj = context.getBean("bB", baseBall.class);
 		System.out.println(obj.getDailyPractice());
 		System.out.println(obj.getThefortune());
 		System.out.println(obj.getName());
